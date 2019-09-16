2195e5e44b4672e6965c856cc38f01e6ca88d6d0
Dexter Freeman <vse.pisma.tut@gmail.com>
2019-09-16 18:42:40 +0300

    Урок 21 сверстал страницу диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,34 @@ import style from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsList}>
+                Dialogs list
+                <div className={style.item + ' ' + style.active}>
+                    User 1
+                </div>
+                <div className={style.item}>
+                    User 2
+                </div>
+                <div className={style.item}>
+                    User 3
+                </div>
+                <div className={style.item}>
+                    User 4
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>
+                    Hello! How are you?
+                </div>
+                <div className={style.message}>
+                    I'm develope my own social network
+                </div>
+                <div className={style.message}>
+                    Wow! It's amazing!
+                </div>
+            </div>
+
         </div>
     )
 }
