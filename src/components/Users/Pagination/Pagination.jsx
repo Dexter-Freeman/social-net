49d7a88a60ba2940f3f1f459bc98c4a3f2a548e1
Dexter Freeman <vse.pisma.tut@gmail.com>
2019-11-12 20:41:34 +0300

    Переделал пагинацию на странице Users, теперь выводится не весь список страниц, а только по 10 страниц и прокрутка на следующие/предыдущие десять

--- src/components/Users/Pagination/Pagination.jsx
+++ src/components/Users/Pagination/Pagination.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Pagination.module.css';
 
-const Pagination = ({totalCount, pageSize, currentPage, setCurrentPage, getUsersFromPage}) => {
+const Pagination = ({totalCount, pageSize, currentPage, setCurrentPage, getUsersFromPage, portionSize = 10}) => {
     const pagesCount = Math.ceil(totalCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     };
 
-    return pages.map((pageNumber) => {
-        return (<span key={pageNumber}
-            onClick={() => {
-                setCurrentPage(pageNumber);
-                getUsersFromPage(pageNumber);
-            }}
-            className={currentPage === pageNumber ? style.currentPage : style.paginationIem}>
-            {pageNumber}
-        </span>)
-    });
+    const portionsCount = Math.ceil(totalCount / portionSize);
+    const [portionNumber, setPortionNumber] = useState(1);
+    const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    const rightPortionPageNumber = portionNumber * portionSize;
+
+    return <div>
+        { portionNumber > 1 ? <button onClick={() => setPortionNumber(portionNumber - 1)}>Prev</button> : `` }
+        { pages.filter((pageNumber) => pageNumber >= leftPortionPageNumber && pageNumber <= rightPortionPageNumber)
+            .map((pageNumber) => {
+                return <span key={pageNumber} 
+                        onClick={() => {
+                            setCurrentPage(pageNumber);
+                            getUsersFromPage(pageNumber);
+                        }} 
+                        className={currentPage === pageNumber ? style.currentPage : style.paginationIem}> 
+                        {pageNumber}
+                    </span>
+            }) }
+        { portionsCount > portionNumber ? <button onClick={() => setPortionNumber(portionNumber + 1)} >Next</button> : `` }
+    </div>
+
+    // return pages.map((pageNumber) => {
+    //     return ({portionNumber > 1 && <button onClick={() => {setPortionNumber(portionNumber - 1)}} >Prev</button> }
+    //     <span key={pageNumber}
+    //         onClick={() => {
+    //             setCurrentPage(pageNumber);
+    //             getUsersFromPage(pageNumber);
+    //         }}
+    //         className={currentPage === pageNumber ? style.currentPage : style.paginationIem}>
+    //         {pageNumber}
+    //     </span>)
+    // });
 };
 
 export default Pagination;
