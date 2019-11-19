ae28aa77c3fa232ba46486cc32e28d36876eadfe
Dexter Freeman <vse.pisma.tut@gmail.com>
2019-11-19 20:45:54 +0300

    Updates

---
+++ precache-manifest.c218250225274ded007acdbf962969ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cbddc6d132175a52cd04b6ffb715ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4b980df4cf1ddb3fea",
+    "url": "/static/css/main.dcbadedf.chunk.css"
+  },
+  {
+    "revision": "effcbf9d6d52406fcc96",
+    "url": "/static/js/2.6129dc9a.chunk.js"
+  },
+  {
+    "revision": "4b4b980df4cf1ddb3fea",
+    "url": "/static/js/main.1cfbd024.chunk.js"
+  },
+  {
+    "revision": "b72a6f17c414ad583d2e",
+    "url": "/static/js/runtime~main.799ed584.js"
+  },
+  {
+    "revision": "42012b3b506a82c453d6064f6cae68b1",
+    "url": "/static/media/preloader.42012b3b.gif"
+  }
+]);
