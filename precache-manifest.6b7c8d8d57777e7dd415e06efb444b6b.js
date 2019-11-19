9ae933e00f54a38ebafc2e4ec8882370f9c159ce
Dexter Freeman <vse.pisma.tut@gmail.com>
2019-11-19 20:53:35 +0300

    Updates

---
+++ precache-manifest.6b7c8d8d57777e7dd415e06efb444b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae716d37341794700996d5108eef245a",
+    "url": "/social-net/index.html"
+  },
+  {
+    "revision": "3da29701a2a36291b194",
+    "url": "/social-net/static/css/main.dcbadedf.chunk.css"
+  },
+  {
+    "revision": "1653f058fb70a2726efd",
+    "url": "/social-net/static/js/2.50b062d5.chunk.js"
+  },
+  {
+    "revision": "3da29701a2a36291b194",
+    "url": "/social-net/static/js/main.d062c895.chunk.js"
+  },
+  {
+    "revision": "035cb81e6a481599c435",
+    "url": "/social-net/static/js/runtime~main.1f64bdf4.js"
+  },
+  {
+    "revision": "42012b3b506a82c453d6064f6cae68b1",
+    "url": "/social-net/static/media/preloader.42012b3b.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c218250225274ded007acdbf962969ee.js"
+  "/social-net/precache-manifest.6b7c8d8d57777e7dd415e06efb444b6b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-net/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
